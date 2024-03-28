4972e299d7ee5d12fba90ae31651efab831c9545
Martin <lefils.martin@gmail.com>
2024-03-28 09:55:03 +0100

    reponse by question

--- src/main/java/com/example/quizify/repository/ReponseRepository.java
+++ src/main/java/com/example/quizify/repository/ReponseRepository.java
@@ -1,10 +1,14 @@
 package com.example.quizify.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.quizify.model.Question;
 import com.example.quizify.model.Reponse;
 
 public interface ReponseRepository extends JpaRepository<Reponse, Integer> {
 	Reponse findById(int id);
 	Reponse findByLibelle(String libelle);
+	List<Reponse> findByQuestion(Question question);
 }
